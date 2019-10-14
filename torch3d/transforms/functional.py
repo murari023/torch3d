d3f273afc20eb7bda67dc5298415f4aeb76df0cb
Quang-Hieu Pham <pqhieu1192@gmail.com>
2019-10-15 07:41:20 +0800

    Tidy up jitter code

--- torch3d/transforms/functional.py
+++ torch3d/transforms/functional.py
@@ -11,7 +11,7 @@ def _is_tensor(tensor):
     return isinstance(tensor, torch.Tensor)
 
 
-def _is_numpy_point_cloud(pcd):
+def _is_numpy_pcd(pcd):
     return pcd.ndim == 2
 
 
@@ -32,10 +32,16 @@ def to_tensor(pcd):
 
 
 def jitter(pcd, sigma, clip):
+    """
+    Random jitter a ``numpy.ndarray`` point cloud with a Gaussian noise.
+
+    """
+
+    if not _is_numpy(pcd):
+        raise TypeError("pcd should be an ndarray. Got {}.".format(type(pcd)))
+
+    if not _is_numpy_pcd(pcd):
+        raise ValueError("pcd should be 2 dimensional. Got {} dimensions.".format(pcd.ndim))
+
     pcd += sigma * np.clip(np.random.rand(pcd.shape), -clip, clip)
     return pcd
-
-
-def rotate(pcd, angle, axis):
-    s = math.sin(angle)
-    c = math.cos(angle)
--- torch3d/transforms/transforms.py
+++ torch3d/transforms/transforms.py
@@ -8,7 +8,7 @@ __all__ = [
     "ToTensor",
     "Shuffle",
     "Downsample",
-    "RandomRotate",
+    "Jitter",
 ]
 
 
@@ -61,7 +61,6 @@ class Downsample(object):
         if self.mode == "random":
             n = len(sample)
             choice = np.random.choice(n, self.num_samples, replace=False)
-
         sample = sample[choice]
         if isinstance(target, np.ndarray):
             target = target[choice]
@@ -75,21 +74,4 @@ class Jitter(object):
 
     def __call__(self, sample, target):
         sample = F.jitter(sample, sigma, clip)
-        return sample, target
-
-
-class Rotate(object):
-    def __init__(self, axis="z"):
-        if axis == "x":
-            self.axis = np.array([1.0, 0.0, 0.0])
-        elif axis == "y":
-            self.axis = np.array([0.0, 1.0, 0.0])
-        elif axis == "z":
-            self.axis = np.array([0.0, 0.0, 1.0])
-        else:
-            self.axis = axis
-
-    def __call__(self, sample, target):
-        angle = np.random.uniform(-np.pi, np.pi)
-        sample = F.rotate(sample, angle, axis)
         return sample, target
