76a2c257859c09dcf5810bd4d98a669106bd0eef
Quang-Hieu Pham <pqhieu1192@gmail.com>
2019-10-17 20:57:08 +0800

    Remove functional transforms

--- torch3d/transforms/transforms.py
+++ torch3d/transforms/transforms.py
@@ -6,11 +6,9 @@ import torch3d.transforms.functional as F
 
 __all__ = [
     "Compose",
-    "First",
-    "Last",
     "ToTensor",
     "Shuffle",
-    "RandomDownsample"
+    "RandomSample"
 ]
 
 
@@ -18,74 +16,30 @@ class Compose(object):
     def __init__(self, transforms):
         self.transforms = transforms
 
-    def __call__(self, *args):
+    def __call__(self, points, target):
         for t in self.transforms:
-            args = t(*args)
-        return args
-
-
-class First(object):
-    def __init__(self, transform):
-        self.transform = transform
-
-    def __call__(self, *args):
-        x = self.transform(args[0])
-        return (x,) + args[1:]
-
-
-class Last(object):
-    def __init__(self, transform):
-        self.transform = transform
-
-    def __call__(self, *args):
-        x = self.transform(args[-1])
-        return args[:-1] + (x,)
-
-
-class Map(object):
-    def __init__(self, transform):
-        self.transform = transform
-
-    def __call__(self, *args):
-        return map(self.transform, args)
+            points, target = t(points, target)
+        return points, target
 
 
 class ToTensor(object):
-    def __call__(self, pcd):
-        return F.to_tensor(pcd)
+    def __call__(self, points, target):
+        points = F.to_tensor(points)
+        return points, target
 
 
 class Shuffle(object):
-    def __init__(self):
-        self.params = None
-
-    @staticmethod
-    def get_params(pcd):
-        num_points = pcd.shape[0]
-        return np.random.permutation(num_points)
-
-    def __call__(self, pcd):
-        if self.params is None:
-            self.params = self.get_params(pcd)
-        perm = self.params
-        # XXX: Should we reset params?
-        self.params = None
-        return pcd[perm]
+    def __call__(self, points, target):
+        n = pcd.shape[0]
+        perm = np.random.permutation(n)
+        return points[perm], target
 
 
-class RandomDownsample(object):
+class RandomSample(object):
     def __init__(self, num_samples):
         self.num_samples = num_samples
-        self.params = None
 
-    @staticmethod
-    def get_params(pcd, num_samples):
-        num_points = pcd.shape[0]
-        return random.sample(range(num_points), num_samples)
-
-    def __call__(self, pcd):
-        if self.params is None:
-            self.params = self.get_params(pcd, self.num_samples)
-        samples = self.params
-        self.params = None
-        return pcd[samples]
+    def __call__(self, points, target):
+        n = pcd.shape[0]
+        samples = random.sample(range(n), num_samples)
+        return points[samples], target
