afcbb1753c0a1b333697ffec8062f4509375d1a4
Quang-Hieu Pham <pqhieu1192@gmail.com>
2019-11-10 19:23:45 +0800

    Update package URL

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     description="Datasets and network architectures for 3D deep learning in PyTorch",
     author="Quang-Hieu Pham",
     author_email="pqhieu1192@gmail.com",
-    url="{}/archive/{}.tar.gz".format(url, __version__),
+    url="{}/archive/v{}.tar.gz".format(url, __version__),
     install_requires=requirements,
     ext_modules=ext_modules,
     cmdclass={"build_ext": BuildExtension},
