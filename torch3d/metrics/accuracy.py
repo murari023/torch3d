Fix divided by zeros in metric calculation

torch3d/metrics/accuracy.py
@@ -58,5 +58,6 @@ class Accuracy(Metric):
         return value.item()
 
     def mean(self):
-        value = torch.mean(self.count / self.total)
+        value = self.count / self.total
+        value = torch.mean(value[torch.isfinite(value)])
         return value.item()

torch3d/metrics/jaccard.py
@@ -24,7 +24,8 @@ class Jaccard(Metric):
             self.union[k] += torch.sum(a | b)
 
     def score(self):
-        value = torch.mean(self.inter / self.union)
+        value = self.inter / self.union
+        value = torch.mean(value[torch.isfinite(value)])
         return value.item()
 
     def mean(self):